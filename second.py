3f41199ed0da8a96dcb83494d0b72bce54d26ce5
nikoaakash13 <aakash.pandey@dogmagroup.co.uk>
2025-12-07 07:52:05 +0545

    changed the name

--- second.py
+++ second.py
@@ -1,7 +1,7 @@
 print('second file')
 print('hello world')
 print('my name is aakash pandey')
-print('my name is rameshowr')
+print('my name is harish')
 print('my name is saurabh')
 
 print('my name is prakash')
